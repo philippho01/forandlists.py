8ad3ac4a5f3c38961f89a25d214786a676025ae3
philippho01 <hofmann2001.ph@gmail.com>
2022-11-15 11:10:53 +0100

    neu

--- übung6.py
+++ übung6.py
@@ -1,4 +1,4 @@
 lst_1 = list(range(0,49))
 
-for i in range(0,49):
-    print(input("Geben Sie eine Zahl ein: "))
+for i in range(6):
+    input...
---
+++ übung8.py
@@ -0,0 +1,3 @@
+for i in range(0,100):
+    for j in range(1, i):
+        print(i, j, j/i)
